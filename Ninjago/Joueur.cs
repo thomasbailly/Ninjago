5c062c17f919a2c50702f5793c4e1aac1688c044
Clement <Clement@THINK-CLEMENT>
2017-10-20 09:42:09 +0200

    Plateau

---
+++ Ninjago/Joueur.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninjago
+{
+    class Joueur
+    {
+        int id;
+        String pseudo;
+        DateTime dateCreation;
+
+        public Joueur(int unId, String unPseudo)
+        {
+            this.id = unId;
+            this.pseudo = unPseudo;
+            this.dateCreation = DateTime.Now;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public String getPseudo()
+        {
+            return this.pseudo;
+        }
+    }
+}
---
+++ Ninjago/classes/CarteVehicule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ninjago.classes
+{
+    class CarteVehicule:Carte
+    {
+        String description;
+
+        public CarteVehicule(String unId, String unNom, int uneQuantite, String uneDescription):base(unId, unNom, uneQuantite)
+        {
+            this.description = uneDescription;
+        }
+    }
+}
